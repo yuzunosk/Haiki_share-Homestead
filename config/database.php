c530eb9dc3d476801d3f1dd9888bb466674b2053
yuzunosk55 <yuzuha3ds@gmail.com>
2020-07-12 07:26:31 +0900

    config database change unix_socket'

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'database' => env('DB_DATABASE', 'Haiki_share'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
-            'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
